8ac8f14da3f0da7784398fda090ec6473057611a
Bree <breehorton25@gmail.com>
2021-10-04 12:11:44 -0400

    add h1 and p

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Here it is!</h1>
+        <p>tra la la la 🔥</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
